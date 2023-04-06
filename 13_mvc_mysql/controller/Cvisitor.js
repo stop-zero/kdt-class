1162eec98c5e497a5217dd6a5a91f1947edf39d5
stop-zero <jungjy0097@naver.com>
2023-04-06 19:54:52 +0900

    mvc_mysql 수정

--- 13_mvc_mysql/controller/Cvisitor.js
+++ 13_mvc_mysql/controller/Cvisitor.js
@@ -29,7 +29,6 @@ exports.postVisitor = (req, res) => {
   });
 };
 
-
 // (4) DELETE /visitor/delete
 exports.deleteVisitor = (req, res) => {
   console.log(req.body); // { id: n }
@@ -39,3 +38,25 @@ exports.deleteVisitor = (req, res) => {
     res.send('삭제 성공!!');
   });
 };
+
+
+
+
+exports.getVisitor = (req, res) => {
+  console.log('*', req.query); // { id: n }
+
+  Visitor.getVisitor(req.query.id, (result) => {
+    console.log('**', result); // model callback에서 넘겨주는 rows[0] => {}
+    res.send(result);
+  });
+};
+
+exports.patchVisitor = (req, res) => {
+  console.log(req.body);
+
+  Visitor.patchVisitor(req.body, () => {
+    res.send('수정 성공!');
+  });
+};
+
+
