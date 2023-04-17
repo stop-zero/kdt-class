5c720cc8fcd0c01d4aaf185a2f4a0385f66bdf1e
stop-zero <jungjy0097@naver.com>
2023-04-17 23:20:37 +0900

    react setting

--- 12_Ex1_mvc/routes/user.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const controller = require('../controller/Cuser');
-const router = express.Router();
-
-router.get('/', controller.main);
-router.post('/practice2', controller.practice2);
-
-module.exports = router;
