d68f4faf215ab4eb06a33de8dcf774504b815341
stop-zero <jungjy0097@naver.com>
2023-04-20 17:29:39 +0900

    react syntheticEvent

---
+++ React/04_eventhandling/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import SyntheticEvent from './syntheticEvent';
+
+function App() {
+  return <div className="App">
+
+    <SyntheticEvent></SyntheticEvent>
+  </div>;
+}
+
+export default App;
