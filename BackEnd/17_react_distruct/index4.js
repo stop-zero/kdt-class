1ba9b5b424ce9669e0afe750fb849334c77c9675
stop-zero <jungjy0097@naver.com>
2023-04-24 17:47:10 +0900

    study reduce

--- BackEnd/17_react_distruct/index4.js
+++ BackEnd/17_react_distruct/index4.js
@@ -73,7 +73,7 @@ const aaa = ['a', 'b', 'c'];
 const bbb = ['d', 'e', 'f'];
 
 // concat(): 배열 합치기
-console.log(aaa.concat(bbb)); //[ 'a', 'b', 'c', 'd', 'e', 'f' ]
+// console.log(aaa.concat(bbb)); //[ 'a', 'b', 'c', 'd', 'e', 'f' ]
 
 const arr1 = [
   { name: 'a', id: 1 },
@@ -95,3 +95,36 @@ const arr3 = arr1.concat(arr2);
 //   { name: 'e', id: 5 },
 //   { name: 'f', id: 6 }
 // ]
+
+//==================================
+// reduce(callback)
+// 배열의 각 요소에 주어진 리듀서 함수를 실행하고, 하나의 결과값을 반환
+const numbers = [1, 2, 3, 4, 5];
+
+// 1 + 2 + 3 + 4 + 5
+const sum = numbers.reduce((acc, currentValue) => {
+  return acc + currentValue;
+});
+console.log(sum); //15
+
+// acc : 누적합 
+const sum2 = numbers.reduce((acc, currentValue, idx) => {
+  console.log(`===${idx}번째 index===`)
+  console.log(`acc : ${acc}`)
+  console.log(`currentValue : ${currentValue}`)
+  return acc + currentValue;
+});
+console.log(sum2); 
+// ===1번째 index===
+// acc : 1
+// currentValue : 2
+// ===2번째 index===
+// acc : 3
+// currentValue : 3
+// ===3번째 index===
+// acc : 6
+// currentValue : 4
+// ===4번째 index===
+// acc : 10
+// currentValue : 5
+// 15
